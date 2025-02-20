fda058d2e9957757cc65ed1f6b72c0542450b0e1
Chin Ilo <chinua_i@yahoo.com>
2025-02-20 14:25:50 -0800

    Update test_greet.py

--- scripts/test_greet.py
+++ scripts/test_greet.py
@@ -4,6 +4,7 @@ def test_greet():
     assert greet("World") == "Hello, World!"
     assert greet("GitHub") == "Hello, GitHub!"
     assert greet("") == "Hello, !"
+    assert greet("1") == "Hello, !"
 
 if __name__ == "__main__":
     import pytest
